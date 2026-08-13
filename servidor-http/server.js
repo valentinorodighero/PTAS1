6dd66015298578c31fff516cc07086320929cec9
Valentino <valentino.rodighero@estudante.ifms.edu.br>
2026-08-13 17:42:00 +0000

    edit: data da requisição adicionada

--- servidor-http/server.js
+++ servidor-http/server.js
@@ -6,6 +6,7 @@ const server = http.createServer();
 
 server.on('request', (req, res) => {
     console.log(`Requisição recebida! ${req.method} ${req.url}`);
+    console.log(new Date().toISOString())
 
     res.statusCode = 201
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');

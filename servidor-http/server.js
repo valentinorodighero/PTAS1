f7462f6c8d4e96a9339326b6e1efa5964e67541e
Valentino <valentino.rodighero@estudante.ifms.edu.br>
2026-08-17 19:56:16 +0000

    ex. 1:  mensagem para as rotas "contato" e "produtos"

--- servidor-http/server.js
+++ servidor-http/server.js
@@ -1,19 +1,40 @@
-const http = require('node:http')
+import http from 'node:http'
+import { url } from 'node:inspector';
+import { URL } from 'node:url'
 
-const porta = 3000
+const port = 3000
 
-const server = http.createServer();
+const status = {
+    "status": "ok",
+    "date": new Date().toISOString()
+}
 
-server.on('request', (req, res) => {
-    console.log(`Requisição recebida! ${req.method} ${req.url}`);
-    console.log(new Date().toISOString())
+const produtos = [
+    { id: 1, nome: "Sabonete" },
+    { id: 2, nome: "Monitor" },
+    { id: 3, nome: "Cadeira Gamer" }
+]
 
-    res.statusCode = 201
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+const server = http.createServer((req, res) => {
+    const urlObj = new URL(req.url, `http://${req.headers.host}`)
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+
+    if (req.method == "GET" && urlObj.pathname == "/contato") {
+        return res.end(JSON.stringify({
+            "numero_telefone": "67 99999-9999",
+            "endereco": "Rua da Alegria, 99"
+        }));
+    }
+
+    if (req.method == "GET" && urlObj.pathname == "/produtos") {
+        return res.end(JSON.stringify(produtos));
+    }
+
+    res.end(JSON.stringify({ "data": "Página Inicial" }));
 });
 
-server.listen(porta, ()=> {
-    console.log(`Servidor ouvindo na porta ${porta}`)
+server.listen(port, () => {
+    console.log("Servidor funcionando na porta ", port)
 });
-
-// O site não carrega, pois a aplicação precisa de uma resposta para mostrar algo na tela
